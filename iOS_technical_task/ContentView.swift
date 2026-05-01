3de9761e7680d9a1960fa0e0192c3630ca6903fa
Anthony Soulier <anthony.soulier@dojo.tech>
2026-05-01 09:54:29 +0100

    Initial Commit

---
+++ iOS_technical_task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_technical_task
+//
+//  Created by Anthony Soulier on 01/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_technical_task/iOS_technical_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_technical_taskApp.swift
+//  iOS_technical_task
+//
+//  Created by Anthony Soulier on 01/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOS_technical_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
